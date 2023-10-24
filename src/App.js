953e61c167154c19cf7212a0bac0a1ddd3a0782c
JohanHauteville <johan.hauteville@gmail.com>
2023-10-24 11:58:01 +0200

    Create components for npm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ReactCardSlider from "./components/ReactCardSlider";
 
 function App() {
+  const sliderClick = (slider) => {
+    alert("Hello World");
+  };
+
+  const slides = [
+    {
+      image: "https://picsum.photos/200/300",
+      title: "This is a title",
+      description: "This is a description",
+      clickEvent: sliderClick,
+    },
+    {
+      image: "https://picsum.photos/600/500",
+      title: "This is a secound title",
+      description: "This is a secound description",
+      clickEvent: sliderClick,
+    },
+    {
+      image: "https://picsum.photos/700/600",
+      title: "This is a third title",
+      description: "This is a third description",
+      clickEvent: sliderClick,
+    },
+    {
+      image: "https://picsum.photos/500/400",
+      title: "This is a fourth title",
+      description: "This is a fourth description",
+      clickEvent: sliderClick,
+    },
+    {
+      image: "https://picsum.photos/300/200",
+      title: "This is a fifth title",
+      description: "This is a fifth description",
+      clickEvent: sliderClick,
+    },
+    {
+      image: "https://picsum.photos/400/300",
+      title: "This is a sixth title",
+      description: "This is a sixth description",
+      clickEvent: sliderClick,
+    },
+    {
+      image: "https://picsum.photos/800/700",
+      title: "This is a seventh title",
+      description: "This is a seventh description",
+      clickEvent: sliderClick,
+    },
+    {
+      image: "https://picsum.photos/400/800",
+      title: "This is a eighth title",
+      description: "This is a eighth description",
+      clickEvent: sliderClick,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="body">
+      <ReactCardSlider slides={slides} />
     </div>
   );
 }
